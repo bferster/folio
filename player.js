9373abcd94ce2a2cda03a02b0c9f0dae595b5a76
Bill Ferster <bferster@virginia.edu>
2016-02-05 13:14:00 -0500

    page editing

--- player.js
+++ player.js
@@ -11,11 +11,11 @@ function player()														// CONSTRUCTOR
 player.prototype.Make=function()										// MAKE PLAYER
 {
 	var str="<div id='playerDiv'  class='sf-player'>";						// Player container
-	str+="<div id='playerTopDiv'  class='sf-playerPane'  contenteditable='true'></div>";				
-	str+="<div id='playerLeftDiv' class='sf-playerPane'  contenteditable='true'></div>";	// Left
-	str+="<div id='playerMidDiv'  class='sf-playerPane'  contenteditable='true'></div>";	// Mid
-	str+="<div id='playerRightDiv' class='sf-playerPane' contenteditable='true'></div>";	// Right
-	str+="<div id='playerBotDiv'  class='sf-playerPane'  contenteditable='true'></div>";	// Bot	
+	str+="<div id='playerPaneTop'  class='sf-playerPane'  contenteditable='true'></div>";				
+	str+="<div id='playerPaneLeft' class='sf-playerPane'  contenteditable='true'></div>";	// Left
+	str+="<div id='playerPaneMid'  class='sf-playerPane'  contenteditable='true'></div>";	// Mid
+	str+="<div id='playerPaneRight' class='sf-playerPane' contenteditable='true'></div>";	// Right
+	str+="<div id='playerPaneBot'  class='sf-playerPane'  contenteditable='true'></div>";	// Bot	
 	return str+"</div>";													// Return player
 }
 
@@ -51,35 +51,40 @@ player.prototype.Update=function(page, defLayout)						// UPDATE PLAYER
 	var mhgt=Math.max(100-tpct-bpct,0);
 	var cpct=Math.max(100-lpct-rpct,0);
 	$('[id^="player"]').show(); 											// Show all
-	$("#playerTopDiv").css({ width:"100%",height:tpct+"%" });
-	$("#playerLeftDiv").css({ width:+lpct+"%",height:mhgt+"%" });
-	$("#playerMidDiv").css({ width:+cpct+"%",height:mhgt+"%"});
-	$("#playerRightDiv").css({ width:+rpct+"%",height:mhgt+"%"});
-	$("#playerBotDiv").css({ width:"100%",height:bpct+"%" });
+	$("#playerPaneTop").css({ width:"100%",height:tpct+"%" });
+	$("#playerPaneLeft").css({ width:+lpct+"%",height:mhgt+"%" });
+	$("#playerPaneMid").css({ width:+cpct+"%",height:mhgt+"%"});
+	$("#playerPaneRight").css({ width:+rpct+"%",height:mhgt+"%"});
+	$("#playerPaneBot").css({ width:"100%",height:bpct+"%" });
+
 	if (!mhgt) {															// No middle's
-		$("#playerLeftDiv").hide();											// Hide them
-		$("#playerMidDiv").hide();
-		$("#playerRightDiv").hide();
+		$("#playerPaneLeft").hide();											// Hide them
+		$("#playerPaneMid").hide();
+		$("#playerPaneRight").hide();
 		}	
-	if (!$("#playerTopDiv").height())	$("#playerTopDiv").hide();			// Hide top if invisible
-	if (!$("#playerLeftDiv").width())	$("#playerLeftDiv").hide();			// Hide left if invisible
-	if (!$("#playerMidDiv").width())	$("#playerMidDiv").hide();			// Hide mid if invisible
-	if (!$("#playerRightDiv").width())	$("#playerRightDiv").hide();		// Hide right if invisible
-	if (!$("#playerBotDiv").height())	$("#playerBotDiv").hide();			// Hide bot if invisible
+	if (!$("#playerPaneTop").height())	$("#playerPaneTop").hide();			// Hide top if invisible
+	if (!$("#playerPaneLeft").width())	$("#playerPaneLeft").hide();			// Hide left if invisible
+	if (!$("#playerPaneMid").width())	$("#playerPaneMid").hide();			// Hide mid if invisible
+	if (!$("#playerPaneRight").width())	$("#playerPaneRight").hide();		// Hide right if invisible
+	if (!$("#playerPaneBot").height())	$("#playerPaneBot").hide();			// Hide bot if invisible
+
 	if (this.ckTop)		this.ckTop.destroy();								// Remove old editors
 	if (this.ckLeft)	this.ckLeft.destroy();								
 	if (this.ckMid)		this.ckMid.destroy();								
 	if (this.ckRight)	this.ckRight.destroy();								
 	if (this.ckTop)		this.ckTop.destroy();								
 	
-	this.ckTop=CKEDITOR.inline( $("#playerTopDiv")[0] );					// Enable rich text
-	this.ckLeft=CKEDITOR.inline( $("#playerLeftDiv")[0] );
-	this.ckMid=CKEDITOR.inline( $("#playerMidDiv")[0] );
-	this.ckRight=CKEDITOR.inline( $("#playerRightDiv")[0] );
-	this.ckBot=CKEDITOR.inline( $("#playerBotDiv")[0] );
-	if (defLayout && (defLayout.panes[0].markUp != undefined))				// If a layout markup set
-		markUp=defLayout.panes[0].markUp;									// Use it
-	$("#playerTopDiv").html(markUp ? markUp : ""); 							// Set html
+	$("#playerPaneTop").html(page.markUpTop ? page.markUpTop : ""); 		// Set html of panes
+	$("#playerPaneLeft").html(page.markUpLeft ? page.markUpLeft : ""); 		
+	$("#playerPaneMid").html(page.markUpMid ? page.markUpMid : ""); 		
+	$("#playerPaneRight").html(page.markUpRight ? page.markUpRight : ""); 		
+	$("#playerPaneBot").html(page.markUpBot ? page.markUpBot : ""); 		
+	
+	this.ckTop=CKEDITOR.inline( $("#playerPaneTop")[0] );					// Enable rich text
+	this.ckLeft=CKEDITOR.inline( $("#playerPaneLeft")[0] );
+	this.ckMid=CKEDITOR.inline( $("#playerPaneMid")[0] );
+	this.ckRight=CKEDITOR.inline( $("#playerPaneRight")[0] );
+	this.ckBot=CKEDITOR.inline( $("#playerPaneBot")[0] );
 }	
 
 
