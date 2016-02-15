d71281b5620434f5fa5edcea660122a00212eed6
Bill Ferster <bferster@virginia.edu>
2016-02-15 12:58:27 -0500

    qmedia save/load

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ function data()															// CONSTRUCTOR
 	this.qmf=new QmediaFile("//qmediaplayer.com/",5);						// Alloc file system
 }	
 
-data.prototype.Save=function(		)									// SAVE
+data.prototype.Save=function()											// SAVE
 {
 	curJson=sf;																// Copy json to global
 	this.qmf.Save(0);
@@ -20,16 +20,11 @@ data.prototype.Load=function()											// LOAD
 
 function LoadShow(data)													// LOAD A SHOW
 {
-	if (data.qmfmsg == "private") {											// If a private file		
-		AlertBox("Private project","Sorry, but this project is marked <i>private</i>. You will need to supply the password to load it");
-		return;																// Quit
-		}
-	else if (data.qmfmsg == "error") {										// If an error
+	if (data.qmfmsg == "error") {											// If an error
 		AlertBox("Sorry, but there was an error loading this project");		// Show
 		return;																// Quit
 		}
-	if (qmf.curFile)														// If a good file
-		curShow=qmf.curFile;												// Set curshow
+	if (qmf.curFile) curShow=qmf.curFile;									// If a good file, set curshow
 	sf.Init(data,"set");													// Init folio
  	Sound("add");															// Add sound
  	sf.Draw();																// Redraw
